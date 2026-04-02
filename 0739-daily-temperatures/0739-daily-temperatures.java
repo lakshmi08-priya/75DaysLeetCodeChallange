fdb0cbbc1897a359ec5b6fdd5fbfc8ffc2d7addb
lakshmi08-priya <lakshmigunisetty5@gmail.com>
2026-04-02 23:59:16 +0530

    Time: 9 ms (96.99%), Space: 105.1 MB (80.49%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -2,25 +2,42 @@ class Solution {
     public int[] dailyTemperatures(int[] t) {
         int n=t.length;
         int[] arr=new int[n];
-        Stack<Integer> s=new Stack<>();
-        for(int i=0;i<n;i++){
-            if(s.isEmpty()){
-                s.push(i);
+        Arrays.fill(arr,0);
+        for(int i=n-2;i>=0;i--){
+            int j=i+1;
+            while(j<n){
+            if(t[i]<t[j]){
+                arr[i]=j-i;
+                break;
+            }
+            else if(arr[j]==0){
+                arr[i]=0;
+                break;
             }
             else{
-                //int val=s.peek();
-                while(!s.isEmpty()&&t[s.peek()]<t[i]){
-                    int val=s.peek();
-                    arr[val]=i-val;
-                    s.pop();
-                }
-                s.push(i);
+                j=j+arr[j];
+            }
             }
         }
-            while(!s.isEmpty()){
-                int val=s.pop();
-                arr[val]=0;
-            }
+        // Stack<Integer> s=new Stack<>();
+        // for(int i=0;i<n;i++){
+        //     if(s.isEmpty()){
+        //         s.push(i);
+        //     }
+        //     else{
+        //         //int val=s.peek();
+        //         while(!s.isEmpty()&&t[s.peek()]<t[i]){
+        //             int val=s.peek();
+        //             arr[val]=i-val;
+        //             s.pop();
+        //         }
+        //         s.push(i);
+        //     }
+        // }
+        //     while(!s.isEmpty()){
+        //         int val=s.pop();
+        //         arr[val]=0;
+        //     }
         return arr;
     }
 }
